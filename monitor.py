f132439792ef04916d09280ea7bb950373171088
Anyaleef <157379674+Anyaleef@users.noreply.github.com>
2026-09-10 16:32:35 +0300

    27

--- monitor.py
+++ monitor.py
@@ -24,7 +24,7 @@ def fetch_page_text() -> str:
     for tag in soup(["script", "style", "nav", "footer", "header"]):
         tag.decompose()
 
-    text = soup.get_text("\n")
+    text = "\n".join(soup.stripped_strings)
     text = re.sub(r"\n+", "\n", text)
     text = re.sub(r"[ \t]+", " ", text)
 
